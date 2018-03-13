acdb834cca1e548cadd7b5ecbace09f012946763
Michał Ołtarzewski <michalo@radcode.co>
2018-03-13 15:47:01 +0100

    Added form

--- veturilo/src/App.jsx
+++ veturilo/src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
+import Input from 'material-ui/Input';
+import Button from 'material-ui/Button';
+import List, { ListItem } from 'material-ui/List';
 
 const paperStyle = {
   marginTop: "10%", 
   marginLeft: "10%", 
   position: "fixed",
   height: "45%",
-  width: "35%",
+  width: "18em",
 };
 
 class App extends Component {
@@ -15,6 +18,8 @@ class App extends Component {
     
     this.state = {
       iconBarHeight: "100%",
+      start: "",
+      destination: "",
     };
   }
   
@@ -29,11 +34,48 @@ class App extends Component {
     });
   }
   
+  onInputChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  }
+  
+  onSubmitClicked = () => {
+    alert("Submit clicked!");
+  }
+  
   render() {
     return (
       <div style={{backgroundImage: "url('map.jpg')", height: this.state.height}}>
-        <Paper style={paperStyle}>
-          
+        <Paper style={paperStyle} display="list-item">
+          <List>
+            <ListItem>
+              <Input
+                style={{marginTop: 20, marginLeft: 20}}
+                placeholder="Początek podróży"
+                onChange={this.onInputChange('start')}
+              />
+            </ListItem>
+            <ListItem>
+              <Input
+                style={{marginLeft: 20}}
+                placeholder="Koniec podróży"
+                onChange={this.onInputChange('destination')}
+              />
+            </ListItem>
+            { this.state.start != "" && this.state.destination != "" &&
+            <ListItem>
+              <Button 
+                style={{marginLeft: "auto", marginRight: "auto"}} 
+                variant="raised" 
+                color="primary"
+                onClick={this.onSubmitClicked}
+              >
+                Szukaj
+              </Button>
+            </ListItem>
+            }
+          </List>
         </Paper>
       </div>
     );
