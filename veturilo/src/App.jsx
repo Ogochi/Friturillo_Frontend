a7977319175d6ad83dfe654316bc4aad99413ece
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2018-03-26 20:39:49 +0200

    Added showing network error modal on stations request error

--- veturilo/src/App.jsx
+++ veturilo/src/App.jsx
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 import List, { ListItem } from 'material-ui/List';
+import { withStyles } from 'material-ui/styles';
+import PropTypes from 'prop-types';
 import converter from 'xml-js';
 import axios from 'axios';
-import AutoComplete from './AutoComplete.jsx'
+import AutoComplete from './AutoComplete.jsx';
+import NetworkErrorModal from './NetworkErrorModal.jsx';
 
-const paperStyle = {
-  marginTop: "10%", 
-  marginLeft: "10%", 
-  position: "fixed",
-  height: "45%",
-  width: "18em",
-  minHeight: "11em"
+const styles = {
+  paper: {
+    marginTop: "10%", 
+    marginLeft: "10%", 
+    position: "fixed",
+    height: "45%",
+    width: "18em",
+    minHeight: "11em"
+  }
 };
 
 class App extends Component {
@@ -24,6 +29,7 @@ class App extends Component {
       start: "",
       destination: "",
       stations: [],
+      networkErrorModalOpen: false,
     };
   }
   
@@ -37,23 +43,37 @@ class App extends Component {
       height:  windowHeight - barHeight,
     });
     
-    axios.get(`http://api.nextbike.net/maps/nextbike-official.xml?city=210`)
-      .then(res => {
-        if (res.statusText === "OK") {
-          let stations = JSON.parse(
-            converter.xml2json(res.data, {compact: true, spaces: 4}))
-              .markers.country.city.place;
-          let stationsNames = stations.map(s => ({
-            name: s._attributes.name
-          }));
+    this.getStations(true)();
+  }
+  
+  getStations = isItFirstTime => () => {
+    if (!isItFirstTime)
+      this.toggleNetworkErrorModal();
 
-          this.setState({
-            stations: stationsNames,
-          });
-        } else {
-          alert("Error - failed to download stations data!");
-        }
-      });
+    axios.get(`http://api.nextbike.net/maps/nextbike-official.xml?city=210`, {
+      timeout: 2000,
+    })
+      .then(res => {
+        let stations = JSON.parse(
+          converter.xml2json(res.data, {compact: true, spaces: 4}))
+            .markers.country.city.place;
+        let stationsNames = stations.map(s => ({
+          name: s._attributes.name
+        }));
+
+        this.setState({
+          stations: stationsNames,
+        });
+      })
+      .catch(() => {
+        this.toggleNetworkErrorModal();
+      })
+  }
+  
+  toggleNetworkErrorModal = () => {
+    this.setState( prevState => ({
+      networkErrorModalOpen: !prevState.networkErrorModalOpen,
+    }));
   }
   
   onInputChange = name => event => {
@@ -67,9 +87,10 @@ class App extends Component {
   }
   
   render() {
+    const { classes } = this.props;
     return (
       <div style={{backgroundImage: "url('map.jpg')", height: this.state.height}}>
-        <Paper style={paperStyle} display="list-item">
+        <Paper className={classes.paper} display="list-item">
           <List style={{marginTop: 20}}>
             <ListItem>
               <AutoComplete
@@ -99,9 +120,19 @@ class App extends Component {
             }
           </List>
         </Paper>
+        <NetworkErrorModal 
+          onClose={this.toggleNetworkErrorModal}
+          onRetry={this.getStations(false)}
+          onCancell={this.toggleNetworkErrorModal}
+          open={this.state.networkErrorModalOpen}
+        />
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
