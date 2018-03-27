99e8acc6ad92c43c3c6115995b5c87737202f9c4
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2018-03-27 16:08:43 +0200

    Added stacions table

--- veturilo/src/Stations.jsx
+++ veturilo/src/Stations.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import Utils from './Utils.js';
 import NetworkErrorModal  from './NetworkErrorModal.jsx';
 
@@ -7,7 +8,8 @@ class Stations extends Component {
     super(props);
     
     this.state = {
-      networkErrorModalOpen: false, 
+      networkErrorModalOpen: false,
+      stations: [],
     };
   }
   
@@ -28,12 +30,46 @@ class Stations extends Component {
   }
   
   onStationsReceived = stations => {
-    console.log(stations);
+    const mappedStations = stations.map(s => ({
+      name: s._attributes.name,
+      bikes: s._attributes.bikes,
+      racks: s._attributes.bike_racks,
+      freeRacks: s._attributes.free_racks,
+      cords: s._attributes.lat + ", " + s._attributes.lng,
+    }));
+
+    this.setState({
+      stations: mappedStations,
+    });
   }
   
   render() {
     return (
       <div>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nazwa stacji</TableCell>
+              <TableCell numeric>Dostępne rowery</TableCell>
+              <TableCell numeric>Ilość stojaków</TableCell>
+              <TableCell numeric>Wolne stojaki</TableCell>
+              <TableCell>Współrzędne</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {this.state.stations.map(s => {
+            return (
+              <TableRow key={s.name + "11"}>
+                <TableCell>{s.name}</TableCell>
+                <TableCell numeric>{s.bikes}</TableCell>
+                <TableCell numeric>{s.racks}</TableCell>
+                <TableCell numeric>{s.freeRacks}</TableCell>
+                <TableCell>{s.cords}</TableCell>
+              </TableRow>
+            );
+          })}
+          </TableBody>
+        </Table>
         <NetworkErrorModal 
           onClose={this.toggleNetworkErrorModal}
           onRetry={this.getStations(false)}
