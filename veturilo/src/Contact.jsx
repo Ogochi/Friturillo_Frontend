1db724bd71a80a417a75b23c3d6a56ab258e06c9
Marcin B <marcin.byra1@gmail.com>
2018-04-05 03:23:49 +0200

    fixed form highlighting in Contact tab, added flex to adjust to different devices, added confirm button (but no logic yet)

--- veturilo/src/Contact.jsx
+++ veturilo/src/Contact.jsx
@@ -1,98 +1,121 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import Input, { InputLabel } from 'material-ui/Input';
+import { InputLabel } from 'material-ui/Input';
 import { MenuItem } from 'material-ui/Menu';
-import { FormControl, FormHelperText } from 'material-ui/Form';
+import { FormControl } from 'material-ui/Form';
 import Select from 'material-ui/Select';
 import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    margin: theme.spacing.unit,
-    minWidth: 400,
-  },
-  formControl: {
-    margin: theme.spacing.unit * 5,
-    minWidth: 400,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing.unit * 2,
-  },
+  	button: {
+    	margin: theme.spacing.unit * 5,
+    	display: 'flex',
+    	maxHeight: 10,
+
+  	},
+  	input: {
+    	display: 'none',
+  	},
+  	root: {
+    	display: 'flex',
+    	flexWrap: 'wrap',
+  	},
+  	textField: {
+	   	fullWidth: true,
+		margin: theme.spacing.unit,
+	    minWidth: 400,
+  	},
+  	formControl: {
+	    margin: theme.spacing.unit * 5,
+	    minWidth: 400,
+  	},
+  	selectEmpty: {
+    	marginTop: theme.spacing.unit * 2,
+  	},
 });
 
 class SimpleSelect extends React.Component {
   state = {
-    age: '',
+    topic: '',
   };
 
-  handleChangeSelect = event => {
+
+  handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-
 
   render() {
     const { classes } = this.props;
 
     return (
-      <form className={classes.root} autoComplete="off">
-        <FormControl className={classes.formControl}>
-          	<InputLabel htmlFor="age-simple">W jakiej sprawie się kontaktujesz?</InputLabel>
-          	<Select
-	            value={this.state.age}
-	            onChange={this.handleChangeSelect}
-	            inputProps={{
-	              name: '',
-	              id: '',
-	            }}
-          	>
-            <MenuItem value="">
-              <em>wybierz temat...</em>
-            </MenuItem>
-            <MenuItem value={10}>Zgłaszam błąd na stronie</MenuItem>
-            <MenuItem value={20}>Potrzebuję pomocy</MenuItem>
-            <MenuItem value={30}>Propozycja współpracy</MenuItem>
-            <MenuItem value={40}>Inne</MenuItem>
+      	<form className={classes.root} autoComplete="off" >
 
-          	</Select>
-          	<TextField
-	            autoFocus
-	          	id="firstName"
-	          	label="Imię"
-	          	type="text"
-              	fullWidth
-              	margin="dense"
+	        <FormControl className={classes.formControl} >
 
-        	/>
-          	<TextField
-	          	id="secondnName"
-	          	label="Nazwisko"
-	          	type="text"
-              	fullWidth
-              	margin="dense"
-        	/>
-        	<TextField
-              	id="email"
-              	label="Email"
-              	type="email"
-              	margin="dense"
-              	fullWidth
-            />
-            <TextField
-              	id="message"
-              	label="Wiadomość"
-              	type="text"
-              	margin="normal"
-              	multiline
-              	rows="6"
-            />
+	          	<InputLabel>W jakiej sprawie się kontaktujesz?</InputLabel>
+	          	<Select
+		            value={this.state.topic}
+		            onChange={this.handleChange}
+		            inputProps={{
+		              	name: 'topic',
+		              	id: 'topic-simple',
+		            }}
+	          	>
+		            <MenuItem value="">
+		              <em>wybierz temat...</em>
+		            </MenuItem>
+		            <MenuItem value={1}>Zgłaszam błąd na stronie</MenuItem>
+		            <MenuItem value={2}>Potrzebuję pomocy</MenuItem>
+		            <MenuItem value={3}>Propozycja współpracy</MenuItem>
+		            <MenuItem value={4}>Inne</MenuItem>
+	          	</Select>
+	        </FormControl>
 
-        </FormControl>
-      </form>
+	        <FormControl className={classes.formControl}>
+	          	<TextField
+		            required
+		          	id="firstName"
+		          	label="Imię"
+		          	type="text"
+	        	/>
+	        </FormControl>
+
+	        <FormControl className={classes.formControl}>
+	        	<TextField
+		          	id="secondName"
+		          	label="Nazwisko"
+		          	type="text"
+	        	/>
+	        </FormControl>
+
+	        <FormControl className={classes.formControl}>	
+	        	<TextField
+	        		required
+	              	id="email"
+	              	label="Email"
+	              	type="email"
+	            />
+	        </FormControl>
+
+	        <FormControl className={classes.formControl}>
+	            <TextField
+	              	required
+	              	id="message"
+	              	label="Wiadomość"
+	              	type="text"
+	              	multiline
+	              	rows="6"
+	            />
+	        </FormControl>
+
+			<Button variant="raised" color="primary" className={classes.button}>
+	       		Wyślij
+	      	</Button>
+
+      	</form>
+
     );
   }
 }
