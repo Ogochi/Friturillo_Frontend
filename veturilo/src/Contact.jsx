c1cedc492b878398a61493d82466cb3aa8aa6858
Marcin B <marcin.byra1@gmail.com>
2018-04-09 23:48:58 +0200

    Contact form is now displayed correctly and adapts to all screen sizes

--- veturilo/src/Contact.jsx
+++ veturilo/src/Contact.jsx
@@ -7,13 +7,15 @@ import { FormControl } from 'material-ui/Form';
 import Select from 'material-ui/Select';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
+import Grid from "material-ui/Grid";
 
 const styles = theme => ({
   	button: {
     	margin: theme.spacing.unit * 5,
     	display: 'flex',
     	maxHeight: 10,
-
+	    minWidth: 270,
+	    maxWidth: 400,
   	},
   	input: {
     	display: 'none',
@@ -25,11 +27,14 @@ const styles = theme => ({
   	textField: {
 	   	fullWidth: true,
 		margin: theme.spacing.unit,
-	    minWidth: 400,
+	    minWidth: 270,
+	    maxWidth: 400,
+
   	},
   	formControl: {
 	    margin: theme.spacing.unit * 5,
-	    minWidth: 400,
+	    minWidth: 270,
+	    maxWidth: 400,
   	},
   	selectEmpty: {
     	marginTop: theme.spacing.unit * 2,
@@ -51,68 +56,71 @@ class SimpleSelect extends React.Component {
 
     return (
       	<form className={classes.root} autoComplete="off" >
+      		<Grid container direction="column" alignItems="center" justify="center">
+		        <Grid item>
+			        <FormControl className={classes.formControl} direction="column">
 
-	        <FormControl className={classes.formControl} >
+			          	<InputLabel>W jakiej sprawie się kontaktujesz?</InputLabel>
+			          	<Select
+				            value={this.state.topic}
+				            onChange={this.handleChange}
+				            inputProps={{
+				              	name: 'topic',
+				              	id: 'topic-simple',
+				            }}
+			          	>
+				            <MenuItem value="">
+				              <em>wybierz temat...</em>
+				            </MenuItem>
+				            <MenuItem value={1}>Zgłaszam błąd na stronie</MenuItem>
+				            <MenuItem value={2}>Potrzebuję pomocy</MenuItem>
+				            <MenuItem value={3}>Propozycja współpracy</MenuItem>
+				            <MenuItem value={4}>Inne</MenuItem>
+			          	</Select>
+			        </FormControl>
+			    </Grid>
 
-	          	<InputLabel>W jakiej sprawie się kontaktujesz?</InputLabel>
-	          	<Select
-		            value={this.state.topic}
-		            onChange={this.handleChange}
-		            inputProps={{
-		              	name: 'topic',
-		              	id: 'topic-simple',
-		            }}
-	          	>
-		            <MenuItem value="">
-		              <em>wybierz temat...</em>
-		            </MenuItem>
-		            <MenuItem value={1}>Zgłaszam błąd na stronie</MenuItem>
-		            <MenuItem value={2}>Potrzebuję pomocy</MenuItem>
-		            <MenuItem value={3}>Propozycja współpracy</MenuItem>
-		            <MenuItem value={4}>Inne</MenuItem>
-	          	</Select>
-	        </FormControl>
+		        <FormControl className={classes.formControl}>
+		          	<TextField
+			            required
+			          	id="firstName"
+			          	label="Imię"
+			          	type="text"
+		        	/>
+		        </FormControl>
 
-	        <FormControl className={classes.formControl}>
-	          	<TextField
-		            required
-		          	id="firstName"
-		          	label="Imię"
-		          	type="text"
-	        	/>
-	        </FormControl>
+		        <FormControl className={classes.formControl}>
+		        	<TextField
+			          	id="secondName"
+			          	label="Nazwisko"
+			          	type="text"
+		        	/>
+		        </FormControl>
 
-	        <FormControl className={classes.formControl}>
-	        	<TextField
-		          	id="secondName"
-		          	label="Nazwisko"
-		          	type="text"
-	        	/>
-	        </FormControl>
+		        <FormControl className={classes.formControl}>	
+		        	<TextField
+		        		required
+		              	id="email"
+		              	label="Email"
+		              	type="email"
+		            />
+		        </FormControl>
 
-	        <FormControl className={classes.formControl}>	
-	        	<TextField
-	        		required
-	              	id="email"
-	              	label="Email"
-	              	type="email"
-	            />
-	        </FormControl>
+		        <FormControl className={classes.formControl}>
+		            <TextField
+		              	required
+		              	id="message"
+		              	label="Wiadomość"
+		              	type="text"
+		              	multiline
+		              	rows="6"
+		            />
+		        </FormControl>
 
-	        <FormControl className={classes.formControl}>
-	            <TextField
-	              	required
-	              	id="message"
-	              	label="Wiadomość"
-	              	type="text"
-	              	multiline
-	              	rows="6"
-	            />
-	        </FormControl>
-
-			<Button variant="raised" color="primary" className={classes.button}>
-	       		Wyślij
-	      	</Button>
+				<Button variant="raised" color="primary" className={classes.button}>
+		       		Wyślij
+		      	</Button>
+		      </Grid>
 
       	</form>
 
