ba82a9023048383a69f529f50b3dba98a461f0ac
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2018-06-05 16:54:53 +0200

    Update consts.js

--- veturilo/src/consts.js
+++ veturilo/src/consts.js
@@ -5,17 +5,17 @@ const consts = {
   stationsApi: `http://${serverIp}:6161/stations`,
   stationsWithBikesApi: `https://api.nextbike.net/maps/nextbike-official.xml?city=210`,
   routeApi: `http://${serverIp}:6161/dijkstra`,
-  networkErrorModalTitle: "Network Connection Error",
-  networkErrorModalContent: `Unfortunately occured network connection error.
-    You can either retry to make a connection or cancel.`,
-  gpsErrorModalTitle: "GPS Error",
-  gpsErrorModalContent: `Geolocations is either not supported by this browser or
-    is currently not available.`,
-  frontPageText: `Friturillo jest portalem umożliwiającym wygodne wyznaczenie takiej
-    trasy przy użyciu sieci rowerów Veturillo, aby uniknąć opłat nawet za
-    długie wycieczki. Serwis umożliwia wyznaczenie trasy na podstawie
-    podanych przez użytkownika, wyznaczonych na podstawie lokalizacji
-    urządzenia lub wskazanych na mapie miejsc startowych i docelowych.`,
+  networkErrorModalTitle: "Błąd połączenia",
+  networkErrorModalContent: `Niestety nie udało się połączyć z siecią. Możesz spróbować ponownie lub anulować.`,
+  gpsErrorModalTitle: "Błąd Geolokacji",
+  gpsErrorModalContent: `Geolokacja nie jest wspierana przez tę przeglądarkę lub nie jest aktualnie dostępna.`,
+  frontPageText: `Friturillo jest portalem umożliwiającym wygodne wyznaczenie przy użyciu warszawskiej 
+    sieci rowerów Veturillo takiej
+    trasy , aby uniknąć opłat nawet za
+    długie wycieczki. Serwis za pomocą
+    podanych przez użytkownika lub ustalonych na podstawie lokalizacji
+    urządzenia miejsca startowego i docelowego wyznacza optymalną trasę wyświetlając następnie odpowiednie
+    detale oraz poglądową mapę.`,
   mobileWebsiteWidth: 600,
 };
 
